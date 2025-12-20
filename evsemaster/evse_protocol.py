99cbe05d9b345333eccf19dd46db08cf2454f790
bastb <bastb@tenberge-ict.nl>
2025-12-20 10:26:53 +0100

    Added disclaimer and did some irrelevant changes

--- evsemaster/evse_protocol.py
+++ evsemaster/evse_protocol.py
@@ -1,4 +1,13 @@
-"""Simple EVSE protocol implementation for Home Assistant integration."""
+"""
+
+This program is distributed in the hope that it will be useful, but is provided AS IS with ABSOLUTELY NO WARRANTY;
+The entire risk as to the quality and performance of the program is with you. Should the program prove defective,
+you assume the cost of all necessary servicing, repair or correction. In no event will any of the developers, or
+any other party, be liable to anyone for damages arising out of the use or inability to use the program.
+You may copy and distribute copies of the Program, provided that you keep intact all the notices that refer to the
+absence of any warranty.
+
+"""
 
 import asyncio
 import logging
@@ -6,8 +15,8 @@ import socket
 import struct
 import zoneinfo
 
-from typing import Any, Optional
 from datetime import datetime
+from typing import Any, Optional
 
 from .data_types import (
     CommandEnum,
@@ -493,3 +502,8 @@ class SimpleEVSEProtocol:
     def is_logged_in(self) -> bool:
         """Check if logged in."""
         return self._logged_in
+
+
+__all__ = [
+    "SimpleEVSEProtocol",
+]
